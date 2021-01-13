5c92a1a74e92f022f9309e0f641b14286236ecb9
Slava-Arest <57600511+Slava-Arest@users.noreply.github.com>
2021-01-13 23:36:42 +0300

    Update LookupArrayService.java

--- LookupArrayService.java
+++ LookupArrayService.java
@@ -2,8 +2,7 @@ package Lecture_7;
 
 public class LookupArrayService {
 
-    public int findMax(int[] array) {
-        int[] elements = {10, 23, 84, 16, 36, 25, 30, 38};
+     public int findMax(int[] array) {
         int max = 0;
         if (array.length > 0) {
             for (int i = 0; i < array.length; i++) max = Math.max(max, array[i]);
@@ -12,7 +11,6 @@ public class LookupArrayService {
     }
 
     public int findMin(int[] array) {
-        int[] elements = {10, 23, 84, 16, 36, 25, 30, 38};
         int min = 10;
         if (array.length > 0) {
             for (int i = 0; i < array.length; i++) min = Math.min(min, array[i]);
@@ -21,10 +19,9 @@ public class LookupArrayService {
     }
 
     public int indexOfMax(int[] array) {
-        int[] elements = {10, 23, 84, 16, 36, 25, 30, 38};
         int idx = 0;
-        for (int i = 0; i < elements.length; i++) {
-            if (elements[idx] < elements[i]) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[idx] < array[i]) {
                 idx = i;
             }
         }
@@ -32,10 +29,9 @@ public class LookupArrayService {
     }
 
     public int indexOfMin(int[] array) {
-        int[] elements = {10, 23, 84, 16, 36, 25, 30, 38};
         int idx = 0;
-        for (int i = 0; i > elements.length; i--) {
-            if (elements[idx] > elements[i]) {
+        for (int i = 0; i > array.length; i--) {
+            if (array[idx] > array[i]) {
                 idx = i;
             }
         }
@@ -45,7 +41,6 @@ public class LookupArrayService {
 
 
     public int indexOfNumber(int[] array, int value) {
-        int[] elements = {10, 23, 84, 16, 36, 25, 30, 38};
         int idx = -1;
         if(array.length>0) {
             for (int i = 0; i < array.length; i++){
@@ -55,3 +50,4 @@ public class LookupArrayService {
         return idx;
     }
     }
+}
